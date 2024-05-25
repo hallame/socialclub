7ef789c1fb9e638721c227583ac92a04d3215576
Hormise ALLAME <104220598+hallame@users.noreply.github.com>
2024-05-25 15:48:59 +0300

    Update

--- Member.cpp
+++ Member.cpp
@@ -1,20 +1,2 @@
 #include "Member.h"
 #include <iostream>
-
-// Member::Member(int id, const std::string &name, const std::string &email) : id(id), name(name), email(email) {}
-//
-// int Member::getId() const {
-//     return id;
-// }
-//
-// std::string Member::getName() const {
-//     return name;
-// }
-//
-// std::string Member::getEmail() const {
-//     return email;
-// }
-//
-// void Member::display() const {
-//     std::cout << "ID: " << id << ", Name: " << name << ", Email: " << email << std::endl;
-// }
